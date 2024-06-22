e66b7194ae974780f8f3d462a566c0a431c3b95b
LingannaJ <jlinganna104@gmail.com>
2024-06-22 19:20:53 +0530

    sg-group

--- 02-sg/data.tf
+++ 02-sg/data.tf
@@ -2,6 +2,3 @@ data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc_id"
 }
 
-data "aws_ssm_parameter" "sg_id" {
-  name = "/${var.project_name}/${var.environment}/sg_id"
-}
--- 02-sg/output.tf
+++ 02-sg/output.tf
@@ -2,6 +2,3 @@ output "db_sg-id" {
     value = module.db
 }
 
-output "sg_name" {
-  value = aws_security_group.allow_tls.id
-}
