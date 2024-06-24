ab0d93a2c0eceaaf3eb83cc52a4ac03d13f9b9e0
LingannaJ <jlinganna104@gmail.com>
2024-06-24 12:44:24 +0530

    sg

--- 05-vpn/main.tf
+++ 05-vpn/main.tf
@@ -2,7 +2,7 @@ resource "aws_key_pair" "vpn" {
   key_name   = "vpn"
   # you can paste the public key directly like this
   # public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVoNeBTZgYJd9rLxJ72jm5MUZjAzc5uSnUECt+taTb1 Dell@DESKTOP-P9SRT12"
-  public_key = file("~/.ssh/openvpn.pub")
+  public_key = file("~/.ssh/openvpn/openvpn.pub")
   # ~ means windows home directory
 }
 
