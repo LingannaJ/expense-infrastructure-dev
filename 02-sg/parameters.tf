8e7fa166c71b11ca545d0bf6e3e2b8e088c6c6d9
LingannaJ <jlinganna104@gmail.com>
2024-06-25 13:15:56 +0530

    backend

--- 02-sg/parameters.tf
+++ 02-sg/parameters.tf
@@ -46,10 +46,10 @@ resource "aws_ssm_parameter" "web_alb_sg_id" {
   value = module.web_alb.sg_id
 }
 
-resource "aws_ssm_parameter" "app_alb_listener_arn" {
-  name  = "/${var.project_name}/${var.environment}/app_alb_listener_arn"
-  type  = "String"
-  value = module.web_alb.sg_id
-}
+# resource "aws_ssm_parameter" "web_alb_listener_arn" {
+#   name  = "/${var.project_name}/${var.environment}/web_alb_listener_arn"
+#   type  = "String"
+#   value = module.web_alb.sg_id
+# }
 
 # app la
